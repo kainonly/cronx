075e9755bda92c6e74774838eda4e0009b0ba78b
kain <zhangtqx@vip.qq.com>
2025-10-22 16:04:18 +0800

    update

--- model/scheduler.go
+++ model/scheduler.go
@@ -1,7 +1,7 @@
 package model
 
 type Scheduler struct {
-	ID       string `gorm:"primaryKey"`
+	ID       string `gorm:"type:varchar;primaryKey"`
 	Status   *bool  `gorm:"not null;default:1"`
 	Name     string `gorm:"type:varchar;not null;uniqueIndex"`
 	Timezone string `gorm:"type:varchar;not null"`
--- sync/main.go
+++ sync/main.go
@@ -32,5 +32,5 @@ func sync(path string) (err error) {
 		return
 	}
 
-	return db.AutoMigrate(model.Scheduler{})
+	return db.AutoMigrate(model.Scheduler{}, model.Job{})
 }
